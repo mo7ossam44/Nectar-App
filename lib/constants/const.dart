998ff82cd32c686bf21953991c49934785ccde0a
Mohamed Hossam <m.mansi2hossam@gmail>
2025-12-21 20:25:32 +0200

    divide main list into 2 lists

---
+++ lib/constants/const.dart
@@ -0,0 +1,43 @@
+import 'package:build_ui/models/product_model.dart';
+
+final List<ProductModel> exclusiveProductList = [
+  ProductModel(
+    name: 'Organic Bananas',
+    image: 'Assets/Images/ph1.png',
+    price: r'$4.99',
+    quantity: '7pcs',
+  ),
+  ProductModel(
+    name: 'Red Apple',
+    image: 'Assets/Images/pngfuel1.png',
+    price: r'$4.99',
+    quantity: '1kg',
+  ),
+  ProductModel(
+    name: 'Organic Bananas',
+    image: 'Assets/Images/ph1.png',
+    price: r'$4.99',
+    quantity: '7pcs',
+  ),
+  ProductModel(
+    name: 'Red Apple',
+    image: 'Assets/Images/pngfuel1.png',
+    price: r'$4.99',
+    quantity: '1kg',
+  ),
+];
+
+final List<ProductModel> bestSellingProductList = [
+  ProductModel(
+    name: 'Bell Pepper Red',
+    image: 'Assets/Images/ph2.png',
+    price: r'$4.99',
+    quantity: '1kg',
+  ),
+  ProductModel(
+    name: 'Ginger',
+    image: 'Assets/Images/pngfuel3.png',
+    price: r'$4.99',
+    quantity: '250gm',
+  ),
+];
---
+++ lib/widgets/best_selling_section.dart
@@ -0,0 +1,47 @@
+import 'package:build_ui/constants/app_font_styles.dart';
+import 'package:build_ui/constants/const.dart';
+import 'package:build_ui/widgets/product_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class BestSellingSection extends StatelessWidget {
+  const BestSellingSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Best Selling',
+                style: AppFontStyles.fontSize24Weight600WithColorBlack,
+              ),
+              Text(
+                'See All',
+                style: AppFontStyles.fontSize16Weight600WithColorGreen,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 12),
+        SizedBox(
+          height: 250,
+          child: ListView.builder(
+            padding: EdgeInsets.only(right: 10),
+            itemCount: bestSellingProductList.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 14.0),
+                child: ProductCard(productModel: bestSellingProductList[index]),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
